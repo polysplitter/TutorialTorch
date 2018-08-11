bccaa8f5559609551793bcbd4c552693bd2868ae
Kyle <polysplitter@gmail.com>
2018-08-11 18:02:49 -0500

    setup person array

--- Torch/CardStack.swift
+++ Torch/CardStack.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 class CardStack: UIView {
+    var cards: [CardView] = []
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         defaultInit()
@@ -13,5 +15,22 @@ class CardStack: UIView {
     
     func defaultInit() {
         self.backgroundColor = .blue
+        
+        for person in people {
+            addPerson(person: person)
+        }
+    }
+    
+    func addPerson(person: Person) {
+        let card = CardView()
+        card.person = person
+        self.addSubview(card)
+        
+        card.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint(item: card, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: card, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: card, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: card, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0).isActive = true
+        self.sendSubviewToBack(card)
     }
 }
--- Torch/CardView.swift
+++ Torch/CardView.swift
@@ -3,7 +3,9 @@ import UIKit
 class CardView: UIView {
     var person: Person! {
         didSet {
-            
+            imageView.image = person.image
+            nameLabel.text = person.name
+            ageLabel.text = String(person.age)
         }
     }
     
@@ -28,5 +30,27 @@ class CardView: UIView {
             v.translatesAutoresizingMaskIntoConstraints = false
             self.addSubview(v)
         }
+        
+        // Image View
+        NSLayoutConstraint(item: imageView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: imageView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: imageView, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: imageView, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0.6, constant: 0).isActive = true
+        
+        // Name Label
+        NSLayoutConstraint(item: nameLabel, attribute: .top, relatedBy: .equal, toItem: imageView, attribute: .bottom, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: nameLabel, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1, constant: 7).isActive = true
+        NSLayoutConstraint(item: nameLabel, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0).isActive = true
+        
+        // Age Label
+        NSLayoutConstraint(item: ageLabel, attribute: .top, relatedBy: .equal, toItem: imageView, attribute: .bottom, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: ageLabel, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1, constant: -7).isActive = true
+        NSLayoutConstraint(item: ageLabel, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0).isActive = true
+        
+        nameLabel.font = UIFont(name: "HelveticaNeue-Light", size: 20)
+        nameLabel.textAlignment = .left
+        
+        ageLabel.font = UIFont(name: "HelveticaNeue-Light", size: 17)
+        ageLabel.textAlignment = .right
     }
 }
