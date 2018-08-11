00268bd788e66f370536b42df4bda09f39bd9925
Kyle <polysplitter@gmail.com>
2018-08-11 15:37:12 -0500

    Initial Commit

---
+++ Torch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Torch
+//
+//  Created by Kyle Hansen on 8/11/18.
+//  Copyright © 2018 proxy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
